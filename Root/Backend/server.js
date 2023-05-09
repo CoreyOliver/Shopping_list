9355dceba33ca3c92068d81593dd4aecf124757a
CoreyOliver <coreyoliver.dev@gmail.com>
2023-05-09 11:05:58 -0400

    backend init

--- Root/Backend/server.js
+++ Root/Backend/server.js
@@ -2,12 +2,15 @@
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const logger = require('morgan')
-const connectDB = require('./config/database')  
+const connectDB = require('./config/database')
+//init routes
+const mainRoutes = require('./routes/main.routes')
 
 //config
 require('dotenv').config({path: './config/.env'})
@@ -21,8 +24,8 @@ require('dotenv').config({path: './config/.env'})
 //db connection
 connectDB()
 
-
-app.set('view engine', 'ejs')
+app.use(cors())
+// app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -34,12 +37,14 @@ app.use(
       secret: process.env.SECRET,
       resave: false,
       saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      store: MongoStore.create({ client: mongoose.connection.getClient() }),
     })
 )
 
 
 //define routes
+
+app.use('/', mainRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)
